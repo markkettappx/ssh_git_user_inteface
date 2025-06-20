47677e465bc5b8214abca2954410d0691301e5d8
markkettappx <markkettapp@gmail.com>
2025-06-20 18:01:49 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,136 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+<div style='background-color: lightblue; border: 1px solid darkblue;'><h1 style='text-align: center;'>Welcome to Your 0.0.0.0 WebServer Management UI</h1></div>
+
+<div style='background-color: pink; border: 1px solid darkblue;'><h1 style='text-align: center;'><i>Server-Phone</i></h1></div>
+
+<p style='text-align: center;'><span style='color: red;'>Warning:</span> Use only one process during a session. </p>
+
+<div style='background-color: lightblue; border: 1px solid darkblue;'><h1 style='text-align: center;'>Stable</h1></div>
+
+<div style='background-color: pink;'><h2 style='text-align: center;'> PHP Server </h2></div>
+<a href=''><img style='display: block; margin: auto; width: 200px;' src='php.png'></img></a>
+
+<div style='text-align: center;'>
+
+<form action='backend.php' method='post' style=''>
+
+<br><br>
+
+<input name='flag' type='hidden' value='php'>
+
+<input name='puerto' placeholder="Puerto">
+
+<br><br>
+
+<input type='submit' value='Abrir 0.0.0.0:<puerto>'>
+
+</form></div>
+
+<div style='background-color: pink;'><h2 style='text-align: center;'> Python Server </h2></div>
+<a href=''><img style='display: block; margin: auto; width: 200px;' src='py.svg'></img></a>
+
+<div style='text-align: center;'>
+
+<form action='backend.php' method='post' style=''>
+
+<br><br>
+
+<input name='flag' type='hidden' value='python'>
+
+<input name='puerto' placeholder="Puerto">
+
+<br><br>
+
+<input type='submit' value='Abrir 0.0.0.0:<puerto>'>
+
+</form></div>
+
+<div style='background-color: pink;'><h2 style='text-align: center;'> Node.js Server </h2></div>
+<a href=''><img style='display: block; margin: auto; width: 200px;' src='nodejs.png'></img></a>
+
+<div style='text-align: center;'>
+
+<form action='' method='' style=''>
+
+<br><br>
+
+<input placeholder="Puerto">
+
+<br><br>
+
+<input type='submit' value='Abrir 0.0.0.0:<puerto>'>
+
+</form></div>
+
+<div style='background-color: pink;'><h2 style='text-align: center;'> Maria Database </h2></div>
+<a href=''><img style='display: block; margin: auto; width: 200px;' src='mdb.svg'></img></a>
+
+<div style='text-align: center;'>
+
+<form action='' method='' style=''>
+
+<br><br>
+
+<input placeholder="Puerto">
+
+<br><br>
+
+<input type='submit' value='Abrir 0.0.0.0:<puerto>'>
+
+</form></div>
+
+<div style='background-color: lightblue; border: 1px solid darkblue;'><h1 style='text-align: center;'>Unstable</h1></div>
+
+<div style='background-color: pink;'><h2 style='text-align: center;'> SSH Server </h2></div>
+<a href=''><img style='display: block; margin: auto; width: 200px;' src='ssh.png'></img></a>
+
+<div style='text-align: center;'>
+
+<form action='' method='' style=''>
+
+<br><br>
+
+<input placeholder="Puerto">
+
+<br><br>
+
+<input type='submit' value='Abrir 0.0.0.0:<puerto>'>
+
+</form></div>
+
+<div style='background-color: lightblue; border: 1px solid darkblue;'><h1 style='text-align: center;'>Repository</h1></div>
+
+<div style='background-color: pink;'><h2 style='text-align: center;'> Git </h2></div>
+<a href=''><img style='display: block; margin: auto; width: 200px;' src='git.png'></img></a>
+
+<div style='text-align: center;'>
+
+<form action='' method='' style=''>
+
+<br><br>
+
+<input placeholder="Puerto">
+
+<br><br>
+
+<input type='submit' value='Abrir 0.0.0.0:<puerto>'>
+
+</form></div>
+
+<div style='background-color: lightblue; border: 1px solid darkblue;'><h1 style='text-align: center;'><h1 style='text-align: center;'>Client Test</h1></div>
+
+<div style='background-color: pink;'><h2 style='text-align: center;'> Navegador FOSS </h2></div>
+
+<div style='text-align: center;'>
+
+<form action='' method='post' style=''>
+
+<input placeholder="URL. Ej: www.url.cl">
+
+</form></div>
+
+<div style='background-color: lightblue; border: 1px solid darkblue;'><h1 style='text-align: center;'><h1 style='text-align: center;'>DNS: Domain Name Server Management</h1></div>
+
+<div style='background-color: pink;'><h2 style='text-align: center;'> NIC </h2></div>
+<div style='text-align: center;'><a style='text-align: center;' href='https://www.nic.cl'>NIC Chile</a></div>
